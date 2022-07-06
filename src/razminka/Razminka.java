bf7031991cccff723f2bdd6b981c3be49bf08649
ialakey <prosoulk2017@gmail.com>
2022-07-06 20:06:35 +0800

    ready

---
+++ src/razminka/Razminka.java
@@ -0,0 +1,47 @@
+package razminka;
+
+import java.util.Scanner;
+
+public class Razminka {
+    
+    public static void main(String[] args) {
+              
+        inputData();
+    }
+   
+
+    public static void inputData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите 3 слова: ");
+        String inputString = sc.nextLine();
+        reverseString(inputString);
+    }
+    
+    public static void reverseString(String inputString) {
+
+        long timeFirst = System.currentTimeMillis();
+        long timeSecond = System.currentTimeMillis();
+        long timeThird = System.currentTimeMillis();
+        String reversedString = new StringBuffer(inputString).reverse().toString();
+        System.out.println("Итоговое слово: " + reversedString);
+
+        for (int i=0; i<1000; i++) {
+          reversedString = new StringBuffer(inputString).reverse().toString();
+        }
+        System.out.println(System.currentTimeMillis() - timeFirst + " - 1000");
+        for (int i=0; i<10000; i++) {
+          reversedString = new StringBuffer(inputString).reverse().toString();
+        }
+        System.out.println(System.currentTimeMillis() - timeSecond + " - 10000");
+        for (int i=0; i<100000; i++) {
+          reversedString = new StringBuffer(inputString).reverse().toString();
+        }
+        System.out.println(System.currentTimeMillis() - timeThird + " - 10000");
+     }
+    
+//    Введите 3 слова: kek re f
+//Итоговое слово: f er kek
+//0 - 1000
+//15 - 10000
+//31 - 10000
+}
